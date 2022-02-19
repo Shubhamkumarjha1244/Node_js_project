35c317ac8af380f41559d30f26128304b2a9f807
shubham kumar jha <jha.kumar.shubham1244@gmail.com>
2022-02-19 10:51:50 +0530

    File Upload

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -12,6 +12,9 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
+
 var config = require('./config');
 
 const mongoose = require('mongoose');
@@ -65,6 +68,7 @@ app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
